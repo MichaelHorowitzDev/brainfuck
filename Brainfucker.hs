60b255503d383061fe6909203666197fff612c8e
MichaelHorowitzDev <michaelhorowitzdev@gmail.com>
2022-04-23 13:09:29 -0400

    add repl from file option to main

--- Brainfucker.hs
+++ Brainfucker.hs
@@ -4,22 +4,23 @@ import System.IO
 
 main :: IO ()
 main = do
-  hSetBuffering stdout NoBuffering
-  putStrLn "What would you like to do?"
-  putStrLn "1 - Compile From File"
-  putStrLn "2 - Compile From Read"
-  putStrLn "3 - REPL"
-  putStrLn "Action: "
-  action <- getChar
-  putChar '\n'
-  case action of
-    '1' -> compileFromFile
-    '2' -> do
-        putStrLn "Enter Code:"
-        code <- getLine
-        compile code
-    '3' -> interpret
-    _ -> do
-      putStrLn "Incorrect command"
-      main
-
+    hSetBuffering stdout NoBuffering
+    putStrLn "What would you like to do?"
+    putStrLn "1 - Compile From File"
+    putStrLn "2 - Compile From Read"
+    putStrLn "3 - REPL"
+    putStrLn "4 - REPL from File"
+    putStrLn "Action: "
+    action <- getChar
+    putChar '\n'
+    case action of
+        '1' -> compileFromFile
+        '2' -> do
+            putStrLn "Enter Code:"
+            code <- getLine
+            compile code
+        '3' -> interpret
+        '4' -> loadFromFile
+        _ -> do
+        putStrLn "Incorrect command"
+        main
